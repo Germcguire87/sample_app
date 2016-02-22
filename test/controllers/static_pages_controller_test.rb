3d5ed53a20cf09cd036dc283cdd9c7a315cf1608
Gerard T McGuire <gtm248@email.vccs.edu>
2016-02-22 17:07:25 -0500

    finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,36 @@
 require 'test_helper'
 
+
+
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+  @text = "| Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@text}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help #{@text}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About #{@text}"
 
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact #{@text}"
   end
 
 end
